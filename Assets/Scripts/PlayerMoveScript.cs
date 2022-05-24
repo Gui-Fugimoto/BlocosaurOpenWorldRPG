8afac2bfc1c2cf7f1d20695ca60b646a47e536f8
Guilherme Fugimoto <guilherme.kfugimoto@gmail.com>
2022-05-24 01:20:36 -0300

    Player Jump and Gravity

--- Assets/Scripts/PlayerMoveScript.cs
+++ Assets/Scripts/PlayerMoveScript.cs
@@ -6,15 +6,36 @@ public class PlayerMoveScript : MonoBehaviour
 {
     public FixedJoystick moveJoy;
     public CharacterController controller;
-    public Transform cam; 
+    public Transform cam;
+    public float jumpHeight = 3f;
 
     public float speed = 6f;
-
+    public float gravity = -10f;
     public float turnSmoothTime = 0.1f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+
     float turnSmoothVelocity;
 
+
+
+    Vector3 velocity;
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
         //float horizontal = Input.GetAxisRaw("Horizontal");
         //float vertical = Input.GetAxisRaw("Vertical");
 
@@ -23,7 +44,11 @@ public class PlayerMoveScript : MonoBehaviour
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        if(direction.magnitude >= 0.1f)
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
@@ -31,6 +56,18 @@ public class PlayerMoveScript : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+
+        }
+
+    }
+    public void JumpOnButtonCommand()
+    {
+        if (isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
     }
+
 }
+
